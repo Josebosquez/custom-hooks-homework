532d8c66de9503c550905de58527bae0046dbeaa
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-08-03 13:45:03 -0600

    email is fully functional

--- custom-hooks-homework/src/App.js
+++ custom-hooks-homework/src/App.js
@@ -1,37 +1,45 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+
+function CustomHooksObjectFormat(initialState) {
+  const [value, setValue] = useState(initialState)
+
+  function onChange(e) {
+    setValue(e.target.value)
+  }
+
+  function clearInput() {
+    setValue('')
+  }
+
+  function showValue() {
+    console.log(value)
+  }
+
+  return { value, onChange, clearInput, showValue }
+}
 
 function App() {
-  function CustomHooksObjectFormat(initialState){
-    const [value, setValue] = useState(initialState)
+  const { value: email, onChange: emailOnChange, clearInput: clearEmailInput, showValue: showEmailValue } = CustomHooksObjectFormat('');
 
-    function onChange(e){
-      setValue(e.target.value)
-    }
-
-
-    return {value, onChange}
-  }
-  const {value: email, onChange: emailOnChange, clearInput: clearEmailInput, showValue: showEmailValue} = CustomHooksObjectFormat('');
-
-  function handleOnSubmit(e){
+  function handleOnSubmit(e) {
     e.preventDefault();
 
-    clearEmailInput();
     showEmailValue()
+    clearEmailInput();
   }
 
   return (
     <div>
       <h1>App-2</h1>
       <form onSubmit={handleOnSubmit}>
-        <input type='email' value = {email}placeholder='email' onChange={(e)=>emailOnChange}/>
-    <br/>
+        <input type='email' value={email} placeholder='email' onChange={(e) => emailOnChange(e)} />
+        <br />
         {/* <input type='text' value = {username}placeholder='username' onChange={(e)=>usernameOnChange}/>
-    <br/>
+        <br/>
 
         <input type='text' value = {password}placeholder='password' onChange={(e)=>passwordOnChange}/> */}
-    <br/>
-    <button type='submit'>Enter</button>
+        {/* <br /> */}
+        <button type='submit'>Enter</button>
       </form>
     </div>
   )
